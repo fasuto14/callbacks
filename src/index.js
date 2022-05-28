4835062f649b854b14861f2707b3a50373f26542
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-05-28 08:06:51 -0300

    Callbacks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css';
+import { buscarHeroe, buscarPlayer } from './js/callbacks';
+const heroeId = 'capi';
+const playerId = 'alvarez';
 
-console.log('Hello world');
+//Callback = funcion mandada como argumento.
+buscarHeroe(heroeId, (heroe) => {
+    console.log(heroe)
+});
+
+buscarPlayer(playerId, (player) => {
+    console.log(player);
+    console.log('Player:');
+})
+
---
+++ src/js/callbacks.js
@@ -0,0 +1,62 @@
+const heroes = {
+    capi: {
+        nombre: 'Capitan America',
+        poder: 'Aguantar inyecciones sin morir'
+    },
+    iron: {
+        nombre: 'Iron Man',
+        poder: 'Absurda cantidad de dinero'
+    },
+    spider: {
+        nombre: 'Spiderman',
+        poder: 'La mejor reaccion alergica a la picadura de insectos'
+    }
+}
+const players = {
+    armani: {
+        nombre: 'Franco',
+        apellido: 'Armani',
+        puesto: 'Arquero',
+        camiseta: '1',
+        sueldo: 1200000,
+    },
+    diaz: {
+        nombre: 'Paulo',
+        apellido: 'Diaz',
+        puesto: 'Defensor Central',
+        camiseta: '2',
+        sueldo: 750000,
+    },
+    perez: {
+        nombre: 'Enzo',
+        apellido: 'Perez',
+        puesto: 'Volante Central',
+        camiseta: '24',
+        sueldo: 1500000,
+    },
+    alvarez: {
+        nombre: 'Julian',
+        apellido: 'Alvarez',
+        puesto: 'Delantero',
+        camiseta: '9',
+        sueldo: 2000000,
+    },
+}
+
+export const buscarHeroe = (id, callback) => {
+    const heroe = heroes[id];
+    callback(heroe);
+}
+
+export const buscarPlayer = (id, callback) => {
+    const player = players[id];
+    callback(player);
+    console.log(
+        `
+         El nombre del player es: ${players[id].nombre} 
+         El apellido del player es: ${players[id].apellido} 
+         El puesto del player es: ${players[id].puesto}
+         La camiseta del player es: ${players[id].camiseta}
+         El sueldo del player es: ${players[id].sueldo} de crocantes   
+        `)
+}
