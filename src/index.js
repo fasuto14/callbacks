62f7eef7a8d9dac6351fb9da8b39f28c5ff7dd63
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-05-28 08:23:47 -0300

    Callback hell problem

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import './styles.css';
 import { buscarHeroe, buscarPlayer } from './js/callbacks';
 const heroeId = 'capi';
-const playerId = 'alvarez2';
+const playerId1 = 'alvarez';
+const playerId2 = 'perez';
+const playerId3 = 'diaz';
 
 //Callback = funcion mandada como argumento.
 buscarHeroe(heroeId, (heroe) => {
     console.log(heroe)
 });
 
-buscarPlayer(playerId, (err, player) => {
-    console.log('Player:');
-    if (err) {
-        console.error(err)
-    } else {
-        console.info(player)
-    }
+buscarPlayer(playerId1, (err, player1) => {
+    if (err) { return console.error(err); }
 
-})
+    buscarPlayer(playerId2, (err, player2) => {
+        if (err) { return console.error(err); }
+        console.log(`Enviando a ${player1.nombre} y a ${player2.nombre} a la cancha!!`)
 
+        buscarPlayer(playerId3, (err, player3) => {
+            if (err) { return console.error(err); }
+            console.log(`Enviando a ${player1.nombre} y a ${player3.nombre} a la cancha!!`)
+        })
+
+    })
+});
